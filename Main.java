b7cc1078b5204e51f985f304c88449ca1a6dc075
flylham <mochraflyilhamsyah@gmail.com>
2025-10-24 15:02:11 +0700

    Perubahan Kedua

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String nama = "Rafly";
         System.out.println("Halo, " + nama + "!");
+        System.out.println("Ini versi kedua dari program Java menggunakan Git.");
+        System.out.println("Belajar commit dan push dengan lancar!");
     }
 }
